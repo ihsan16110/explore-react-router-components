e7a2d02ac24c542c1b18a100b0d9ef8057412c51
Mahdi Ihsan <mahdiihsan16110@gmail.com>
2023-09-19 01:01:49 +0600

    First Commit for this Project

---
+++ src/components/Contacts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <div>
+            <h2>This is the Contacts Page</h2>
+        </div>
+    );
+};
+
+export default Contacts;
